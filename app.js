a20e366ab6ef69eec051313b6ed69706083ba274
X738173 <stephane.le-minh-nhut@socgen.com>
2024-07-07 20:44:03 +0200

    CORS +npm install mongodb +connection to mongoose +environment variable recovery with dotenv

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+// Charger les variables d'environnement depuis le fichier .env
+dotenv.config();
+
+// Récupérer le mot de passe depuis les variables d'environnement
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://sam121110987654321:${password}@cluster16.vssydqk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster16`,
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+    
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
+
 
 module.exports = app;
