cf43988e5e55bd06c79cae772e7bedaad4e3959c
X738173 <stephane.le-minh-nhut@socgen.com>
2024-07-07 21:18:26 +0200

    updated controller and route to redirect to a specific book

--- controllers/books.js
+++ controllers/books.js
@@ -10,3 +10,10 @@ exports.getAllBooks = (req, res, next) => {
             res.status(500).json({ message: 'Fetching books failed!' });
         });
 };
+
+// redirection vers un livre spécifique
+exports.getOneBook = (req, res, next) => {
+    Book.findOne({ _id: req.params.id })
+        .then(book => res.status(200).json(book))
+        .catch(error => res.status(404).json({ error }));
+};
--- routes/books.js
+++ routes/books.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const booksCtrl = require('../controllers/books');
 
 router.get('/', booksCtrl.getAllBooks);
+router.get('/:id', booksCtrl.getOneBook);
 
 module.exports = router;
