5a89e2125532c632fa1d103bc30a21205ea57f4d
X738173 <stephane.le-minh-nhut@socgen.com>
2024-07-07 21:47:37 +0200

    update app.js file

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req, res, next) => {
     // La méthode hash() de bcrypt crée un hash crypté des mots de passe des utilisateurs pour les enregistrer de manière sécurisée dans la base de données
@@ -23,6 +24,7 @@ User.findOne({ email: req.body.email })
         if (!user) {
             return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
+        // La méthode compare de bcrypt compare un string avec un hash pour, par exemple, vérifier si un mot de passe entré par l'utilisateur correspond à un hash sécurisé enregistré en base de données. Cela montre que même bcrypt ne peut pas décrypter ses propres hashs.
         bcrypt.compare(req.body.password, user.password)
             .then(valid => {
                 if (!valid) {
@@ -30,6 +32,7 @@ User.findOne({ email: req.body.email })
                 }
                 res.status(200).json({
                     userId: user._id,
+                    // La fonction sign de jsonwebtoken permet de chiffrer un nouveau token
                     token: jwt.sign(
                         { userId: user._id },
                         'RANDOM_TOKEN_SECRET',
